e2a9418fbbff686382640288933bbd8c10b5f4f7
Ganijon <ganijon.rahimov@gmail.com>
2017-01-24 18:32:32 -0600

    Added AddBookScreen files

---
+++ ui/Library.UI/src/library/ui/addbook/AddBookScreenController.java
@@ -0,0 +1,22 @@
+package library.ui.addbook;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author 985565
+ */
+public class AddBookScreenController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
