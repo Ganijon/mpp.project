37461ab79514816fb0261c957616e67d83bce7ad
Ganijon <ganijon.rahimov@gmail.com>
2017-01-25 13:03:47 -0600

    Fixed fxml files

--- ui/library.ui/src/App.java
+++ ui/library.ui/src/App.java
@@ -6,17 +6,23 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
-        
-       Parent root = FXMLLoader.load(getClass().getResource("library/ui/LoginScreen.fxml"));
-        
+
+        Parent root = FXMLLoader.load(getClass().getResource("library/ui/login/LoginScreen.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("library/ui/home/HomeScreen.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("library/ui/addbook/AddBookScreen.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("library/ui/addcopy/AddCopyScreen.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("library/ui/addmember/AddMemberScreen.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("library/ui/overdue/OverdueScreen.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("library/ui/printcheckout/PrintCheckoutScreen.fxml"));
+
         Scene scene = new Scene(root);
-        
+
         stage.setScene(scene);
         stage.show();
-        
+
     }
 
     /**
@@ -25,5 +31,5 @@ public class App extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
