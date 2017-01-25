d4276c3e82f96c4589d1c2736de5b7243921a019
Ganijon <ganijon.rahimov@gmail.com>
2017-01-25 12:32:56 -0600

    Moved Overdue to another package

---
+++ ui/library.ui/src/library/ui/overdue/OverdueController.java
@@ -0,0 +1,17 @@
+package library.ui.overdue;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class OverdueController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
