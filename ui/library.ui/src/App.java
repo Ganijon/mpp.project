17d1f2dad8de7d6c0d02d6cc3ddf2a5c7b83ecb5
Ganijon <ganijon.rahimov@gmail.com>
2017-01-25 12:32:23 -0600

    Added App class

--- ui/library.ui/src/App.java
+++ ui/library.ui/src/App.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package library.ui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -11,11 +5,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author 985484
- */
-public class Main extends Application {
+public class App extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
