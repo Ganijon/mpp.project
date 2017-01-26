bf389601cf44447b005fbe9b9c9773bdb02490ef
Ganijon <ganijon.rahimov@gmail.com>
2017-01-26 15:41:46 -0600

    Minor changes

--- ui/library.ui/src/UserContext.java
+++ ui/library.ui/src/UserContext.java
@@ -8,8 +8,8 @@ public class UserContext {
     private static UserAccount currentUser;
 
     static {
-        data.put("lib", new UserAccount("lib", "lib", Access.Librarian));
         data.put("admin", new UserAccount("admin", "admin", Access.Admin));
+        data.put("lib", new UserAccount("lib", "lib", Access.Librarian));
         data.put("both", new UserAccount("both", "both", Access.Both));
     }
 
